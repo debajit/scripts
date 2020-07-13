2e19abc1ccb3883e3134010672b1e73f03b25676
Debajit Adhikary <debajit@debajit.com>
2020-07-13 01:50:33 -0700

    Simplify and cleanup TiddlyWiki scripts

--- wiki.sh
+++ wiki.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 #
+# TODO: Update docs
+#
 # start-wiki - Start a TiddlyWiki server locally
 #
 # SYNOPSIS
@@ -18,11 +20,31 @@
 
 source "notification.sh"
 
+PERSONAL_WIKI_ROOT="${HOME}/Documents/wiki"
+PERSONAL_WIKI_PORT=4444
+
+WORK_WIKI_ROOT="${HOME}/WorkDocs/Documents/wiki"
+WORK_WIKI_PORT=5555
+
 function start_wiki {
   wiki_dir="${1}"
-  port=${2:-4444}
+  port=${2}
 
   echo_notify "Starting TiddlyWiki server on port ${PORT} from '${wiki_dir}'..."
   send_notification "TiddlyWiki" "Serving '${wiki_dir}' on port ${port}"
   tiddlywiki "${wiki_dir}" --server "${port}"
+}
+
+function watch_wiki {
+  wiki_dir="${1}"
+
+  cd "${wiki_dir}"
+  if [[ ! -d "tiddlers" ]]; then
+    echo_error "This script needs to be run from inside a TiddlyWiki directory"
+    exit 1
+  fi
+
+  echo_notify "Watching wiki. Changes will be uploaded upstream automatically."
+  send_notification "Wiki Watcher" "Watching TiddlyWiki at '${wiki_dir}'"
+  watchexec -p -d 3000 -i '$__StoryList.tid' -- git-pushall
 }
