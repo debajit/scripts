a1b359e747f74ddb8d33c0ddc9acac93f7915e2a
Your Full Name Here <debajit@debajit.com>
2020-11-02 18:20:45 -0800

    Refactor backup-book-category script using backup.sh functions
    This lays the foundation to allow future backup scripts to be created
    and maintainted easily

--- backup.sh
+++ backup.sh
@@ -13,6 +13,17 @@ function backup_dir {
 
   assert_dir_exists "${local_dir}"
 
-  echo_notify "Backing up to ${remote}..."
-  rclone -P "${local_dir}" "${remote}:${remote_dir}"
+  local local_dir_basename=$( basename "${local_dir}" )
+  echo_notify "Backing up '${local_dir_basename}' to '${remote}:${remote_dir}'..."
+
+  if rclone -P copy "${local_dir}" "${remote}:${remote_dir}"; then
+    echo_notify "Successfully backed up '${local_dir_basename}' to ${remote}"
+    # osascript -e 'display notification "✅ Uploaded changes" with title "Wiki Watcher" sound name "Submarine"'
+    send_notification 'RClone' "Backed up '${local_dir_basename}' to '${remote}'"
+  else
+    echo_error "Failed to backup '${local_dir_basename}' to ${remote}:${remote_dir}"
+    send_notification 'RClone' "Failed to backup '${local_dir_basename}' to '${remote}:${remote_dir}'"
+    # TODO: Add icon to notification
+    # --icon=dialog-error
+  fi
 }
