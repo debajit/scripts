095adb4227fcc3a8b38170d2d1f18d13ed9c516b
Debajit Adhikary <debajit@debajit.com>
2020-12-21 21:32:27 -0800

    Add generic backup script to start replacing specifialized backup script
    This would make it easy to configure all the backup locations from a
    single place, and run backups very easily, for both local and cloud
    backups.

--- backup.sh
+++ backup.sh
@@ -7,22 +7,22 @@
 #   backup_dir cmus tulip "Debajit/Backups/cmus"
 #
 function backup_dir {
-  local local_dir="${1}"
-  local remote="${2}"
-  local remote_dir="${3}"
+  local -r given_local_dir="${1}"
+  local -r given_remote="${2}"
+  local -r given_remote_dir="${3}"
 
-  assert_dir_exists "${local_dir}"
+  assert_dir_exists "${given_local_dir}"
 
-  local local_dir_basename=$( basename "${local_dir}" )
-  echo_notify "Backing up '${local_dir_basename}' to '${remote}:${remote_dir}'..."
+  local local_dir_basename=$( basename "${given_local_dir}" )
+  echo_notify "Backing up '${local_dir_basename}' to '${given_remote}:${given_remote_dir}'..."
 
-  if rclone --exclude=".*" -P copy "${local_dir}" "${remote}:${remote_dir}"; then
-    echo_notify "Successfully backed up '${local_dir_basename}' to ${remote}"
+  if rclone --exclude=".*" -P copy "${given_local_dir}" "${given_remote}:${given_remote_dir}"; then
+    echo_notify "Successfully backed up '${local_dir_basename}' to ${given_remote}"
     # osascript -e 'display notification "✅ Uploaded changes" with title "Wiki Watcher" sound name "Submarine"'
-    send_notification 'RClone' "Backed up '${local_dir_basename}' to '${remote}'"
+    send_notification 'RClone' "Backed up '${local_dir_basename}' to '${given_remote}'"
   else
-    echo_error "Failed to backup '${local_dir_basename}' to ${remote}:${remote_dir}"
-    send_notification 'RClone' "Failed to backup '${local_dir_basename}' to '${remote}:${remote_dir}'"
+    echo_error "Failed to backup '${local_dir_basename}' to ${given_remote}:${given_remote_dir}"
+    send_notification 'RClone' "Failed to backup '${local_dir_basename}' to '${given_remote}:${given_remote_dir}'"
     # TODO: Add icon to notification
     # --icon=dialog-error
   fi
