863bedc404b49a34e5968b43ed989dd14ebb96d2
Debajit Adhikary <debajit@debajit.com>
2024-07-14 17:16:24 -0700

    notification.sh: Add checkmark icon

--- notification.sh
+++ notification.sh
@@ -1,6 +1,6 @@
 # See also https://unix.stackexchange.com/a/438357/141850
 function echo_notify {
-  echo -e "$(tput bold)$(tput setaf 154)➥ $(tput setaf 49)${1}$(tput sgr0)"
+  echo -e "✅ $(tput bold)$(tput setaf 154)$(tput setaf 49)${1}$(tput sgr0)"
 }
 
 function echo_error {
